bd5831e99486fb49f6020ed7f13a1018289395be
csteinmetz1 <csteinmetz1@gmail.com>
2021-02-05 10:09:27 -0500

    making the mlp larger, adding layer norm, and a tanh at the output

--- iirnet/mlp.py
+++ iirnet/mlp.py
@@ -9,8 +9,8 @@ class MLPModel(IIRNet):
     def __init__(self, 
                 num_points = 512,
                 num_layers = 10,
-                hidden_dim = 128,
-                order = 2,
+                hidden_dim = 2048,
+                max_order = 2,
                 normalization = "none",
                 lr = 3e-4,
                 **kwargs):
@@ -29,11 +29,11 @@ class MLPModel(IIRNet):
             else:
                 self.layers.append(torch.nn.Sequential(
                     torch.nn.Linear(in_features, out_features),
-                    torch.nn.BatchNorm1d(out_features, momentum=0.001),
+                    torch.nn.LayerNorm(out_features),
                     torch.nn.PReLU(),
                 ))
 
-        n_coef = (self.hparams.order//2) * 6
+        n_coef = (self.hparams.max_order//2) * 6
         self.layers.append(torch.nn.Linear(out_features, n_coef))
 
         if self.hparams.normalization == "bn":
@@ -52,14 +52,18 @@ class MLPModel(IIRNet):
             x = layer(x) 
 
         # reshape into sos format (n_section, (b0, b1, b2, a0, a1, a2))
-        n_sections = self.hparams.order//2
+        n_sections = self.hparams.max_order//2
         x = x.view(-1,n_sections,6)
+
+        x = torch.tanh(x)
 
         return x
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=self.hparams.lr)
+        #optimizer = torch.optim.Adam(self.parameters(), lr=self.hparams.lr)
+        optimizer = torch.optim.SGD(self.parameters(), lr=self.hparams.lr)
         lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=4, verbose=True)
+        #lr_scheduler = torch.optim.lr_scheduler.CyclicLR(optimizer, base_lr=1e-5, max_lr=1e-3, verbose=True)
         return {
             'optimizer': optimizer,
             'lr_scheduler': lr_scheduler,
@@ -73,7 +77,7 @@ class MLPModel(IIRNet):
         # --- model related ---
         parser.add_argument('--num_points', type=int, default=512)
         parser.add_argument('--num_layers', type=int, default=4)
-        parser.add_argument('--hidden_dim', type=int, default=1024)
+        parser.add_argument('--hidden_dim', type=int, default=4096)
         parser.add_argument('--max_order', type=int, default=10)
         parser.add_argument('--normalization', type=str, default="none")
         # --- training related ---
