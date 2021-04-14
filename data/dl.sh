aeb8d5d8bb94dae5784b704712f622e102a8c07e
csteinmetz1 <csteinmetz1@gmail.com>
2021-04-14 16:54:00 -0400

    updates to script that grabs hrtf to also convert to 16/44.1 wav

--- data/dl.sh
+++ data/dl.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 mkdir HRTF
+cd HRTF
 for i in $(seq -f "%04g" 1002 1059)
 do
+  echo IRC_$i
   wget ftp://ftp.ircam.fr/pub/IRCAM/equipes/salles/listen/archive/SUBJECTS/IRC_$i.zip
   unzip IRC_$i.zip -d IRC_$i/
-  mv IRC_$i/ HRTF/IRC_$i
   rm IRC_$i.zip
+  cd IRC_$i/COMPENSATED/WAV
+  find . -name "*.wav" -exec ffmpeg -i {} -acodec pcm_s16le {}_16.wav -loglevel panic \;
+  cd ../../..
 done
 
