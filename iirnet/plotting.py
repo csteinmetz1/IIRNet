fede8b26a4db36e02d8366568cd0bd729e2baf20
csteinmetz1 <csteinmetz1@gmail.com>
2021-04-27 17:26:42 -0400

    new plot function for eval.py

--- iirnet/plotting.py
+++ iirnet/plotting.py
@@ -1,5 +1,6 @@
 import os
 import io
+import torch
 import PIL.Image
 import numpy as np
 import scipy.signal
@@ -148,3 +149,81 @@ def plot_compare_response(pred_coef, target_coef, num_points=512, eps=1e-8, fs=4
     plt.close("all")
 
     return image
+
+def plot_responses(pred_sos, target_dB, filename=None):
+
+    mag_idx = 0
+    #phs_idx = 1
+    plot_idx = 1
+
+    fig, axs = plt.subplots(nrows=1, ncols=2, figsize=(9, 3))
+
+    zeros,poles,k = scipy.signal.sos2zpk(pred_sos.squeeze())
+    w_pred, h_pred = signal.sosfreqz(pred_sos, worN=target_dB.shape[-1], fs=44100)
+    mag_pred = 20 * torch.log10(h_pred.abs() + 1e-8) 
+
+    axs[mag_idx].plot(w_pred, target_dB, color='tab:blue', label="target")
+    axs[mag_idx].plot(w_pred, mag_pred.squeeze(), color='tab:red', label="pred")
+    #axs[mag_idx].plot(w_pred, mag_pred - target_dB, color='tab:green', label="error")
+
+    axs[mag_idx].set_xscale('log')
+    axs[mag_idx].set_ylim([-60, 40])
+    axs[mag_idx].grid()
+    axs[mag_idx].spines['top'].set_visible(False)
+    axs[mag_idx].spines['right'].set_visible(False)
+    axs[mag_idx].spines['bottom'].set_visible(False)
+    axs[mag_idx].spines['left'].set_visible(False)
+    axs[mag_idx].set_ylabel('Amplitude (dB)')
+    axs[mag_idx].set_xlabel('Frequency (Hz)')
+    axs[mag_idx].legend()
+
+    #axs[phs_idx].plot(w_pred, np.squeeze(np.angle(h_pred)), color='tab:red', label="pred")
+    #axs[phs_idx].plot(w_pred, target_h_ang, color='tab:blue', label="target")
+    #axs[phs_idx].plot(w_pred, target_h_ang, color='tab:blue', label="target")
+    #axs[phs_idx].set_xscale('log')
+    #axs[phs_idx].set_ylim([-60, 40])
+    #axs[phs_idx].grid()
+    #axs[phs_idx].spines['top'].set_visible(False)
+    #axs[phs_idx].spines['right'].set_visible(False)
+    #axs[phs_idx].spines['bottom'].set_visible(False)
+    #axs[phs_idx].spines['left'].set_visible(False)
+    #axs[phs_idx].set_ylabel('Angle (radians)')
+
+    # pole-zero plot
+    for pole in poles:
+        axs[plot_idx].scatter(
+                        np.real(pole), 
+                        np.imag(pole), 
+                        c='tab:red', 
+                        s=10, 
+                        marker='x', 
+                        facecolors='none')
+    for zero in zeros:
+        axs[plot_idx].scatter(
+                        np.real(zero), 
+                        np.imag(zero), 
+                        s=10, 
+                        marker='o', 
+                        facecolors='none', 
+                        edgecolors='tab:red')
+
+    # unit circle
+    unit_circle = circle1 = plt.Circle((0, 0), 1, color='k', fill=False)
+    axs[plot_idx].add_patch(unit_circle)
+    axs[plot_idx].set_ylim([-1.5, 1.5])
+    axs[plot_idx].set_xlim([-1.5, 1.5])
+    axs[plot_idx].grid()
+    axs[plot_idx].spines['top'].set_visible(False)
+    axs[plot_idx].spines['right'].set_visible(False)
+    axs[plot_idx].spines['bottom'].set_visible(False)
+    axs[plot_idx].spines['left'].set_visible(False)
+    axs[plot_idx].set_aspect('equal') 
+    axs[plot_idx].set_axisbelow(True)
+    axs[plot_idx].set_ylabel("Im")
+    axs[plot_idx].set_xlabel('Re')
+
+    plt.tight_layout()
+    if filename is not None:
+        plt.savefig(f'./data/plots/{filename}.png')
+
+    plt.close('all')
