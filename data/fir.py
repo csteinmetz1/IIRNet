584a3f3ab0f1640137cd688a5181d3d913f7fa80
csteinmetz1 <csteinmetz1@gmail.com>
2021-04-28 18:11:24 -0400

    add a function to load magnitude response from file

--- data/fir.py
+++ data/fir.py
@@ -5,6 +5,47 @@ import torch
 import torchaudio
 import scipy.signal
 
+def get_ir_magnitude(filename, num_points=512, n_fft=65536, eps=1e-8):
+    
+    h, sr = torchaudio.load(filename, normalize=True)
+
+    if h.shape[0] > 1:
+        # left channel
+        h = h[0,:]
+
+    # zero pad or crop
+    if h.shape[-1] < n_fft:
+        padsize = (n_fft - h.shape[-1])//2
+        h = torch.nn.functional.pad(h, (padsize,padsize))
+    else:
+        h = h[:n_fft]
+
+    # apply Hann window
+    wn = torch.hann_window(h.shape[-1])
+    h *= wn
+
+    H = torch.fft.rfft(h, n=n_fft)
+
+    # compute dB magnitude
+    mag = 20 * torch.log10(H.abs() + eps)
+
+    mag = torch.nn.functional.interpolate(
+        mag.view(1,1,-1), 
+        size=num_points+1
+    ).view(-1)
+
+    # omit nyquist bin
+    mag = mag[:num_points]
+
+    # smooth
+    mag = scipy.signal.savgol_filter(mag.numpy(), 41, 2)
+    mag = torch.tensor(mag)
+
+    # zero mean
+    mag = mag - torch.mean(mag)
+
+    return mag
+
 class FIRFilterDataset(torch.utils.data.Dataset):
     """ Dataset class to load FIR filters. 
     
@@ -12,11 +53,13 @@ class FIRFilterDataset(torch.utils.data.Dataset):
     def __init__(self, 
             data_dir,
             num_points = 512,
+            n_fft = 65536,
             eps = 1e-8
         ):
         super(FIRFilterDataset, self).__init__()
 
         self.num_points = num_points
+        self.n_fft = n_fft
         self.eps = eps
 
         # get a list of all files 
@@ -34,24 +77,11 @@ class FIRFilterDataset(torch.utils.data.Dataset):
 
         filename = self.files[idx]
 
-        h, sr = torchaudio.load(filename, normalize=True)
-
-        H = torch.fft.rfft(h, n=self.num_points*2)
-
-        # left channel
-        H = H[0,:]
-
-        # omit nyquist bin
-        H = H[:self.num_points]
-
-        # compute dB magnitude
-        mag = 20 * torch.log10(H.abs() + self.eps)
-
-        # smooth
-        mag = scipy.signal.savgol_filter(mag.numpy(), 41, 2)
-        mag = torch.tensor(mag)
-
-        # zero mean
-        mag = mag - torch.mean(mag)
+        mag = get_ir_mag(
+                filename, 
+                num_points=self.num_points, 
+                n_fft=self.n_fft,
+                eps=self.eps
+            )
 
         return mag, None, None, None, None
