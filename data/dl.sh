ea78cac58464b2f6fcc6bbad6b3f0158bc05617b
csteinmetz1 <csteinmetz1@gmail.com>
2021-10-04 21:49:39 +0100

    new download script for IR datasets

--- data/dl.sh
+++ data/dl.sh
@@ -1,14 +1,28 @@
-#!/bin/bash
+wget ftp://ftp.ircam.fr/pub/IRCAM/equipes/salles/listen/archive/SUBJECTS/IRC_1059.zip
+unzip IRC_1059.zip
 mkdir HRTF
-cd HRTF
-for i in $(seq -f "%04g" 1002 1059)
-do
-  echo IRC_$i
-  wget ftp://ftp.ircam.fr/pub/IRCAM/equipes/salles/listen/archive/SUBJECTS/IRC_$i.zip
-  unzip IRC_$i.zip -d IRC_$i/
-  rm IRC_$i.zip
-  #cd IRC_$i/COMPENSATED/WAV
-  #find . -name "*.wav" -exec ffmpeg -i {} -acodec pcm_s16le {}_16.wav -loglevel panic \;
-  #cd ../../..
+mv COMPENSATED/WAV/IRC_1059_C/ HRTF/
+rm -rf RAW/ COMPENSATED/ 
+rm IRC_1059.zip
+# convert to 16-bit 44.1 kHz
+for f in $(find ./HRTF -name '*.wav'); do 
+echo $f
+ffmpeg -i $f -acodec pcm_s16le -ar 44100 ${f%.wav}z.wav; 
+rm $f;
+mv ${f%.wav}z.wav $f;
 done
 
+wget http://skaldir.bplaced.net/bilda/KalthallenCabsIR.rar
+7z x KalthallenCabsIR.rar 
+mkdir GtrCab
+mv "KalthallenCabsIR/Kalthallen IRs" ./GtrCab
+rm -rf KalthallenCabsIR
+rm KalthallenCabsIR.zip
+
+# convert to 16-bit 44.1 kHz
+for f in $(find ./GtrCab -name '*.wav'); do 
+echo $f
+ffmpeg -i $f -acodec pcm_s16le -ar 44100 "${f%.wav}z.wav"; 
+rm $f;
+mv ${f%.wav}z.wav $f;
+done
