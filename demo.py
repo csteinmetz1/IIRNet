929367bc970680bf4959b66cc0629a7fad3dceb1
csteinmetz1 <csteinmetz1@gmail.com>
2021-10-07 20:38:13 +0100

    updates to demo

--- demo.py
+++ demo.py
@@ -1,6 +1,6 @@
+import torch
 import numpy as np
 import scipy.signal
-import torch
 import matplotlib.pyplot as plt
 from iirnet.designer import Designer
 
@@ -8,20 +8,30 @@ from iirnet.designer import Designer
 designer = Designer()
 
 n = 32  # Desired filter order (4, 8, 16, 32, 64)
-m = [0, -3, 0, 6, 0, -3, 0]  # Magnitude response specification
+m = [0, -3, 0, 12, 0, -6, 0]  # Magnitude response specification
+mode = "linear"  # interpolation mode for specification
 output = "sos"  # Output type ("sos", or "ba")
 
 # now call the designer with parameters
-sos = designer(n, m, output=output)
+sos = designer(n, m, mode=mode, output=output)
 
 # measure and plot the response
 w, h = scipy.signal.sosfreqz(sos.numpy(), fs=2)
 
 # interpolate the target for plotting
 m_int = torch.tensor(m).view(1, 1, -1).float()
-m_int = torch.nn.functional.interpolate(m_int, 512)
+m_int = torch.nn.functional.interpolate(m_int, 512, mode=mode)
 
-plt.plot(w, 20 * np.log10(np.abs(h)))
-plt.plot(w, m_int.view(-1))
+fig, ax = plt.subplots(figsize=(6, 3))
+plt.plot(w, 20 * np.log10(np.abs(h)), label="Estimation")
+plt.plot(w, m_int.view(-1), label="Specification")
+ax.spines["top"].set_visible(False)
+ax.spines["bottom"].set_visible(False)
+ax.spines["left"].set_visible(False)
+ax.spines["right"].set_visible(False)
+plt.ylabel("dB")
+plt.xlabel("Normalized frequency")
 plt.grid(c="lightgray")
-plt.savefig("demo.png")
+plt.legend()
+plt.tight_layout()
+plt.savefig("demo.svg")
